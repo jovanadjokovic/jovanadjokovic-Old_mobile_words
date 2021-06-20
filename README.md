1c8debdb10c094637bd846734baf961ba7e1c7cf
jdjokovic <jdjokovic@nextgl.com>
2021-06-20 21:35:04 +0200

    phone photo added

---
+++ README.md
@@ -0,0 +1,3 @@
+This app returns only real English words.
+
+Heroku link: https://djokovic-old-mobile-words.herokuapp.com
---
+++ front/app.js
@@ -0,0 +1,44 @@
+//let canvas = document.getElementById('mobileCanvas')
+//let ctx = c.getContext('2d')
+//ctx.moveTo(window.getInnerWidth/2,500)
+
+let stage = new createjs.Stage('mobileCanvas')
+window.stage = stage
+createjs.Touch.enable(stage)
+
+let mobile = new Image();
+mobile.src = "images/Nokia_3310.png"
+mobile.onload = handleImageLoad
+
+
+function handleImageLoad(event) {
+    var image = event.target;
+    var bitmap = new createjs.Bitmap(image);
+    bitmap.scaleX=0.5
+    bitmap.scaleY=0.5
+    stage.addChild(bitmap);
+    stage.update();
+}
+
+
+//  let bitmap = new createjs.Bitmap(mobile);
+// stage.addChild(bitmap);
+// stage.update();
+
+
+// var queue = new createjs.LoadQueue();
+// queue.on("complete", function(event) {
+//     var image = queue.getResult("image");
+//     var bmp = new createjs.Bitmap(image);
+//     // Do stuff with bitmap
+// });
+// queue.loadFile({src:"./images/Nokia_3310.png", id:"nokia"});
+
+// let mobile = new createjs.Bitmap('./images/Nokia_3310.png')
+// mobile.x = 500
+// mobile.y=500
+// mobile.image.onload = function() {
+//     stage.update();
+// }
+
+//stage.addChild(mobile)
