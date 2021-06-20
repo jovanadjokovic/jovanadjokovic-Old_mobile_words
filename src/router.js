7f05aed64c0904f83c0d8ea4643cb58e1e27e3bc
jdjokovic <jdjokovic@nextgl.com>
2021-06-20 19:09:42 +0200

    backend that returns only real words

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const router = new express.Router()
 
+const wordlist = require('wordlist-english')
+let englishWords = wordlist['english']
+
 // const dictionaryMap = require('./dictionary')
 const combinations = require('./combinations')
 
 router.post('/generate-words', async (req, res) => {
+    //console.log(englishWords)
     
     try {
         const numbers = await req.body.numbers
         let words = combinations(numbers.toString(), [])
-        res.send(words)
+         const realWords = words.filter(word => englishWords.indexOf(word)>-1)
+
+        res.send(realWords)
 
     } catch (e) {
         res.status(404).send()
