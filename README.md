f428b9283ae0f9f94253db21bf2ba96fac9ffcac
jdjokovic <jdjokovic@nextgl.com>
2021-06-21 18:07:10 +0200

    buttons added to canvas

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This app returns only real English words.
+Frontend is done using Canvas and CreateJS.
 
 Heroku link: https://djokovic-old-mobile-words.herokuapp.com
--- front/app.js
+++ front/app.js
@@ -5,8 +5,12 @@
 let stage = new createjs.Stage('mobileCanvas')
 window.stage = stage
 createjs.Touch.enable(stage)
+let numbers=[]
 
-let mobile = new Image();
+let word = ''
+let addGraphics = () => {
+
+    let mobile = new Image();
 mobile.src = "images/Nokia_3310.png"
 mobile.onload = handleImageLoad
 
@@ -21,24 +25,149 @@ function handleImageLoad(event) {
 }
 
 
-//  let bitmap = new createjs.Bitmap(mobile);
-// stage.addChild(bitmap);
-// stage.update();
+let audio = new Audio('./sounds/click.mp3');
 
 
-// var queue = new createjs.LoadQueue();
-// queue.on("complete", function(event) {
-//     var image = queue.getResult("image");
-//     var bmp = new createjs.Bitmap(image);
-//     // Do stuff with bitmap
-// });
-// queue.loadFile({src:"./images/Nokia_3310.png", id:"nokia"});
+let addNumber = (x, y, number) => {
+    
+    let button = new Image();
+    button.src = "images/"+number+".png"
+    //button.src = "images/icon.png"
+    button.onload = (event) => {
+        var image = event.target;
+        var bitmap = new createjs.Bitmap(image)
+        bitmap.x=x
+        bitmap.y=y
+        bitmap.scaleX=0.38
+        bitmap.scaleY=0.38
+        bitmap.name = number 
+        
+        numbers.push(bitmap)
+        stage.addChild(bitmap)
+        stage.update()
+        bitmap.addEventListener("click", () => {
+            audio.play();
+            createjs.Tween.get(bitmap).to({scaleX: 0.37, scaleY: 0.37}, 100).call(() => {
+                stage.update()
+                createjs.Tween.get(bitmap).to({scaleX: 0.4, scaleY:0.4}, 200).call(() => {
+                    stage.update()
+                    createjs.Tween.get(bitmap).to({scaleX: 0.38, scaleY: 0.38}, 50).call(() => {
+                        stage.update()
+                    })
+                })
+            })
+            
+            word+=bitmap.name
 
-// let mobile = new createjs.Bitmap('./images/Nokia_3310.png')
-// mobile.x = 500
-// mobile.y=500
-// mobile.image.onload = function() {
-//     stage.update();
-// }
+            
+        })
+    }
+}
 
-//stage.addChild(mobile)
+
+
+let y=455
+let x=28
+
+addNumber(x, y, 1)
+addNumber(x+79, y+10, 2)
+addNumber(x+170, y, 3)
+addNumber(x, y+45, 4)
+addNumber(x+78, y+62, 5)
+addNumber(x+170, y+50, 6)
+addNumber(x, y+95, 7)
+addNumber(x+88, y+107, 8)
+addNumber(x+165, y+103, 9)
+
+
+
+let eraseButton= new Image();
+eraseButton.src = "images/erase.png"
+eraseButton.onload = handleEraseLoad
+
+
+function handleEraseLoad(event) {
+    var image = event.target;
+    var bitmap = new createjs.Bitmap(image);
+    bitmap.x=29 
+    bitmap.y=370 
+    bitmap.scaleX=0.4
+    bitmap.scaleY=0.4
+    stage.addChild(bitmap);
+    stage.update();
+    bitmap.addEventListener("click", () => {
+        audio.play();
+        createjs.Tween.get(bitmap).to({scaleX: 0.37, scaleY: 0.37}, 100).call(() => {
+            stage.update()
+            createjs.Tween.get(bitmap).to({scaleX: 0.4, scaleY:0.4}, 200).call(() => {
+                stage.update()
+                createjs.Tween.get(bitmap).to({scaleX: 0.38, scaleY: 0.38}, 50).call(() => {
+                    stage.update()
+                })
+            })
+        })
+        word=word.substring(0,word.length-1)
+        
+        
+    })
+}
+
+let blueButton= new Image();
+blueButton.src = "images/enter.png"
+blueButton.onload = handleBlueLoad
+
+
+function handleBlueLoad(event) {
+    var image = event.target;
+    var bitmap = new createjs.Bitmap(image);
+    bitmap.x=79  
+    bitmap.y=355 
+    bitmap.scaleX=0.4
+    bitmap.scaleY=0.4
+    stage.addChild(bitmap);
+    stage.update();
+    bitmap.addEventListener("click", () => {
+        audio.play();
+        createjs.Tween.get(bitmap).to({scaleX: 0.37, scaleY: 0.37}, 100).call(() => {
+            stage.update()
+            createjs.Tween.get(bitmap).to({scaleX: 0.4, scaleY:0.4}, 200).call(() => {
+                stage.update()
+                createjs.Tween.get(bitmap).to({scaleX: 0.38, scaleY: 0.38}, 50).call(() => {
+                    stage.update()
+                })
+            })
+        })
+        
+        // fetch('localhost:3000/generate-words', {
+        //     method: 'POST',
+        //     body: JSON.stringify(word),
+        // }).then(response => response.json())
+        // .then(data => {
+        //   console.log('Success:', data);
+        // })
+        
+        // .then (response => {
+        //     console.log(response)
+        //     word = ''
+        // })
+        //console.log()
+        //word = ''
+        
+})
+
+}
+}
+
+addGraphics()
+
+
+dismissAll = function () {
+
+    for (let i; i < this.children.length; i++) {
+        createjs.Tween.removeTweens(this.children[i]);
+      }
+  
+
+}
+
+//dismissAll()
